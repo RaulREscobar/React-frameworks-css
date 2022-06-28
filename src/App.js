04eb1f31eba0072c864c5ddea47371f30d6fde2f
RaulREscobar <raul.r.escobar@hotmail.com.ar>
2022-06-27 22:35:36 -0300

    im´plementacion de react-bootstrap y material-ui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import Bootstrap from './components/Bootstrap';
-import Bulma from './components/Bulma';
+import MaterilUI from './components/MaterialUI';
+//import Bootstrap from './components/Bootstrap';
+//import Bulma from './components/Bulma';
+import ReactBootstrap from './components/ReactBootstrap';
 
 
 function App() {
   return (
     <div>
-     <h1>Frameworks CSS con React</h1>
-     {/* <Bootstrap /> */}
-     <Bulma />
+      <h1>Frameworks CSS con React</h1>
+      {/* <Bootstrap /> */}
+      {/* <Bulma /> */}
+      {/* <ReactBootstrap /> */}
+      <MaterilUI />
     </div>
   );
 }
---
+++ src/components/MaterialUI.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MediaCard from './MaterialCard';
+import PersistentDrawerLeft from './MaterialDrower';
+
+export default function MaterilUI() {
+    return (
+        <>
+            <h2>Material UI</h2>
+            <MediaCard />
+            <PersistentDrawerLeft />
+        </>
+    )
+}
